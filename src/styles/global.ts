74d8f15c2e4f8cb4d1e8f637a18dc9c670aae2b6
José Camargo <josecamargo@Joses-MacBook-Pro.local>
2021-05-04 23:27:57 -0300

    storybook configured

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-size: 62.5%;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+`
+
+export default GlobalStyles
